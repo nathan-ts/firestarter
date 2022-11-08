64c67736218928ebdc3fff05d97a3c0fa6bd4eba
Nathan Tsang <nathan.tsang@vretta.com>
2022-11-08 12:47:12 -0500

    set up router for home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
-  { path: '' }
+  { path: '', component: HomePageComponent }
 ];
 
 @NgModule({
