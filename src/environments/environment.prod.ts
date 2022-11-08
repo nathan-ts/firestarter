fcad2b3d6ad756bc42e9ef6c51c4eb07c8ed0693
Nathan Tsang <nathan.tsang@vretta.com>
2022-11-08 15:59:27 -0500

    add firebase cred

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAKB0ZR8P9diXiGNr9ZsUUnQwW_hHD6jMA',
+    authDomain: 'kanban-nt.firebaseapp.com',
+    projectId: 'kanban-nt',
+    storageBucket: 'kanban-nt.appspot.com',
+    messagingSenderId: '138650123478',
+    appId: '1:138650123478:web:2bec208725c436f0a59d77',
+    measurementId: 'G-7F1GZTVNZW',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAKB0ZR8P9diXiGNr9ZsUUnQwW_hHD6jMA',
+    authDomain: 'kanban-nt.firebaseapp.com',
+    projectId: 'kanban-nt',
+    storageBucket: 'kanban-nt.appspot.com',
+    messagingSenderId: '138650123478',
+    appId: '1:138650123478:web:2bec208725c436f0a59d77',
+    measurementId: 'G-7F1GZTVNZW',
+  },
 };
 
 /*
