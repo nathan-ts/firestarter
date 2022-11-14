8c0d4177747b32188dfce6d56568816798e88980
Nathan Tsang <nathan.tsang@vretta.com>
2022-11-14 12:34:58 -0500

    add angular fire auth to update login/profile view in navbar based on login state

--- src/app/shared/shell/shell.component.ts
+++ src/app/shared/shell/shell.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-shell',
@@ -16,7 +17,7 @@ export class ShellComponent implements OnInit {
       shareReplay() // template subscribes more than once using async pipe, ensures all listeners get same value
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, public afAuth: AngularFireAuth) {}
 
   ngOnInit(): void {}
 }
